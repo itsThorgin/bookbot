9ea7307e1084c06b5d56dca12b37b1e61f7f79b2
itsThorgin <160652369+itsThorgin@users.noreply.github.com>
2024-12-02 15:53:40 +0100

    added character counter for books

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     path_to_book = "books/frankenstein.txt"
     book_text = get_book_text(path_to_book)
     number_of_words = get_number_of_words(book_text)
+    char_count = get_number_of_characters(book_text)
     print(f"This book has {number_of_words} words.")
+    print(f"These are the characters used in the book: {char_count}")
 
 def get_book_text(path_to_book):
     with open(path_to_book) as book:
@@ -12,5 +14,14 @@ def get_book_text(path_to_book):
 def get_number_of_words(book_text):
     words = book_text.split()
     return len(words)
+
+def get_number_of_characters(book_text):
+    words_to_lower = book_text.lower()
+    dictionary = {}
+
+    for char in words_to_lower:
+        count = dictionary.get(char, 0)
+        dictionary[char] = count + 1
+    return dictionary
 
 main()
